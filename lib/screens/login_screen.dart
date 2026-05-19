939e43cde0a32dd21b85b8039dae9732993c5d2c
some-randomGuy03 <norbertkvs12@gmail.com>
2026-05-20 01:39:50 +0300

    Final V1 version

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -377,7 +377,7 @@ class _LoginScreenState extends State<LoginScreen>
                               OutlinedButton.icon(
                                 onPressed: () async {
                                   final uri = Uri.parse(
-                                    'https://github.com/some-randomGuy03/pontaj_mobile/releases/download/1.2.1/app-release.apk',
+                                    'https://github.com/some-randomGuy03/pontaj_mobile/releases/download/1.2.2/app-release.apk',
                                   );
                                   if (await canLaunchUrl(uri)) {
                                     await launchUrl(
